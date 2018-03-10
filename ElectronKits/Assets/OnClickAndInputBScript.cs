2e7aa85926f53238a29c9218fa8544add8013919
POH_jonathan <pohjonathan@gmail.com>
2018-03-10 21:40:59 +0800

    Added Logic Gate Features
    Added Logic Gate Features

---
+++ ElectronKits/Assets/OnClickAndInputBScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickAndInputBScript : MonoBehaviour {
+
+public TextMesh inputA_current, inputA_next;
+private bool bInput_current = true, bInput_next = false;
+public static int iInput_current, iInput_next;
+
+public AudioSource click_sound;
+
+void Start()
+{
+	convertToNum();
+	inputA_current.text = iInput_current.ToString();
+	inputA_next.text = iInput_next.ToString();
+}
+
+void OnMouseDown()
+{
+
+
+	bInput_current = !bInput_current;
+	bInput_next = !bInput_next;
+
+	convertToNum();
+	inputA_current.text = iInput_current.ToString();
+	inputA_next.text = iInput_next.ToString();
+
+	click_sound.Play();
+
+}
+
+void convertToNum()
+{
+	if (bInput_current == true)
+	{
+		iInput_current = 1;
+	}
+	else
+	{
+		iInput_current = 0;
+	}
+
+	if (bInput_next == true)
+	{
+		iInput_next = 1;
+	}
+
+	else
+	{
+		iInput_next = 0;
+	}
+
+	}
+
+}
