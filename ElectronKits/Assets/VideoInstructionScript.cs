90d95a3d41b42fecf20cca740df9cf5bdeed193a
POH_jonathan <pohjonathan@gmail.com>
2018-03-30 16:42:47 +0800

    Added Video Instructions

---
+++ ElectronKits/Assets/VideoInstructionScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VideoInstructionScript : MonoBehaviour {
+
+	public GameObject videoPlayer, instructionPanel;
+	public static bool videoCard = false;
+	// Use this for initialization
+	void Start () {
+
+		videoPlayer.SetActive(false);
+instructionPanel.SetActive(false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		videoCard = CustomResistorTracker.oneKresistorTracker;
+		if (videoCard == true)
+		{
+			videoPlayer.SetActive(true);
+			instructionPanel.SetActive(false);
+		}
+
+		else
+		{
+
+			videoPlayer.SetActive(false);
+			instructionPanel.SetActive(true);
+		}
+
+	}
+}
